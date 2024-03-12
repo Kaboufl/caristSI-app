93ff07194093064858c9294c0e2ef7dcfc10907f
Aurélien VERNAY <aurevernay@yahoo.fr>
2024-03-12 08:22:58 +0100

    WIP resolve VM factory integration to Login Activity

--- app/src/main/java/org/esicad/btssio2aslam/caristsi/caristsi/data/LoginRepository.kt
+++ app/src/main/java/org/esicad/btssio2aslam/caristsi/caristsi/data/LoginRepository.kt
@@ -1,13 +1,14 @@
 package org.esicad.btssio2aslam.caristsi.caristsi.data
 
 import org.esicad.btssio2aslam.caristsi.caristsi.data.model.LoggedInUser
+import javax.inject.Inject
 
 /**
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
  */
 
-class LoginRepository(val dataSource: LoginDataSource) {
+class LoginRepository @Inject constructor(val dataSource: LoginDataSource) {
 
     // in-memory cache of the loggedInUser object
     var user: LoggedInUser? = null
